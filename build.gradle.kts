3b21f0e4bc645a938c3507b266827186092ca968
gecko10000 <levtey@gmail.com>
2025-04-21 03:50:11 -0500

    Add Nexo as repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://redempt.dev/")
+    maven("https://repo.nexomc.com/releases")
 }
 
 dependencies {
